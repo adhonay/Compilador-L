f162d78fe7cdb4abb72b403abe6f98a1957a6ad2
Ådhonay junior <junioradhonay.gmail.com>
2019-04-07 13:22:12 -0300

    Teste versionamento.

---
+++ Compilador-L/Compilador-L/LC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador_L
+{
+    class LC
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Versionamento.");
+            Console.ReadKey();
+        }
+    }
+}
