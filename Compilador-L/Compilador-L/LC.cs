da3582107ac9241a5e092501d12f5f990b2e846c
Ådhonay junior <junioradhonay.gmail.com>
2019-04-13 16:13:11 -0300

    inicio

--- Compilador-L/Compilador-L/LC.cs
+++ Compilador-L/Compilador-L/LC.cs
@@ -1,17 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Pontifícia Universidade Católica de Minas Gerais
+ * Compilador
+ * Autores: Adhonay Júnior, Izabela Costa
+ * Matricula: 504656, 498535
+ **/
 namespace Compilador_L
 {
     class LC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teste Versionamento.");
-            Console.ReadKey();
+            Console.WriteLine("Digite o nome e extensão do arquivo:");
+            var arquivo = Console.ReadLine();
+
+            if (File.Exists(arquivo))
+            {
+                //if (arquivo.(size - 2) == '.' && (arquivo.charAt(size - 1) == 'l' || arquivo.charAt(size - 1) == 'L'))
+                //{
+                    Stream entrada = File.Open(arquivo, FileMode.Open);
+
+                StreamReader leitor = new StreamReader(entrada);
+                string linha = leitor.ReadLine();
+                while (linha != null)
+                {
+                    Console.WriteLine(linha);
+                    linha = leitor.ReadLine();
+                }
+                leitor.Close();
+                entrada.Close();
+
+                Console.WriteLine("Precione Enter para sair.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(arquivo + " não existe!");
+            }
         }
     }
 }
---
+++ Compilador-L/Compilador-L/TabelaSimbolos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador_L
+{
+    public class TabelaSimbolos
+    {
+
+        public Hashtable TabelaSim()
+        {
+            
+            Hashtable tabela = new Hashtable();
+            tabela.Add("teste", "ds");
+            tabela.Add("adhonay", 2);
+            return tabela;
+            
+        }
+        
+        
+    }
+}
