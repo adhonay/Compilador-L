5fe09679345113b990e90c2031957b86c0245c8b
izabe <izabe@LAPTOP-R7OSPI5V>
2019-04-13 18:28:08 -0300

    simbolos

---
+++ Compilador-L/Compilador-L/Simbolos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador_L
+{
+    /*
+     * classe para armazenamento dos simbolos.Os simbolos tem as seguintes informações: 
+     * token
+     * endereco
+     * lexemas (são retirados do código fonte)
+    */
+    class Simbolos
+    {
+        private byte token { get; set; }
+        private String lexema { get; set; }
+        private int endereco { get; set; }
+        //private byte categoria { get; set; }
+        //private byte tipo { get; set; }
+
+        public Simbolos(String lexema, byte token)
+        {
+            this.lexema = lexema;
+            this.token = token;
+            this.endereco = 0;
+           // this.category = NO_CATEGORY;
+           // this.type = NO_TYPE;
+        }
+       
+    }
+
+
+
+}
