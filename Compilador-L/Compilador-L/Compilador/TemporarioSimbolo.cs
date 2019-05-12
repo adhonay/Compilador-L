0e327c7728d64190fbe65d316a7688a19451ab7e
izabe <izabe@LAPTOP-R7OSPI5V>
2019-05-12 20:22:59 -0300

    semanticas regras

---
+++ Compilador-L/Compilador-L/Compilador/TemporarioSimbolo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador_L.Compilador
+{
+	class TemporarioSimbolo
+	{
+		private byte tipo;
+		private byte classe;
+
+		public TemporarioSimbolo()
+		{
+			this.tipo = 0;
+			this.classe = 0;
+		}
+
+		public TemporarioSimbolo(byte tipo, byte classe)
+		{
+			this.tipo = tipo;
+			this.classe = classe;
+
+		}
+
+		public byte getTipo()
+		{
+			return tipo;
+		}
+
+		public void setTipo(byte tipo)
+		{
+			this.tipo = tipo;
+		}
+
+		public byte getClasse()
+		{
+			return classe;
+		}
+
+		public void setClasse(byte classe)
+		{
+			this.classe = classe;
+		}
+
+
+
+	}
+}
